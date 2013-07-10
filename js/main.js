120887d056daf6626d8aa103804c113ec41b25ae
sysmn <sysmn.pls+git@gmail.com>
2013-07-10 16:11:47 +0900

    ファイル追加

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/**
+ * enchant();
+ * Preparation for using enchant.js.
+ * (Exporting enchant.js class to global namespace.
+ *  ex. enchant.Sprite -> Sprite etc..)
+ *
+ * enchant.js を使う前に必要な処理。
+ * (enchant.js 本体や、読み込んだプラグインの中で定義されている enchant.Foo, enchant.Plugin.Bar などのクラスを、
+ *  それぞれグローバルの Foo, Bar にエクスポートする。)
+ */
+enchant();
+
+/*
+ * window.onload
+ *
+ * The function which will be executed after loading page.
+ * Command in enchant.js such as "new Core();" will cause an error if executed before entire page is loaded.
+ *
+ * ページがロードされた際に実行される関数。
+ * すべての処理はページがロードされてから行うため、 window.onload の中で実行する。
+ * 特に new Core(); は、<body> タグが存在しないとエラーになるので注意。
+ */
+window.onload = function(){
+
+};
