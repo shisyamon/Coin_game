cbc1d376cf1f3deffe0cd4346463f1d9f339f2b7
sysmn <sysmn.pls+git@gmail.com>
2013-08-19 03:08:00 +0900

    キャラクターの表示及び地面や壁との衝突判定を追加

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@
 enchant();
 
 var IMG_GROUND = 'res/ground_test.png';
+var IMG_PLAYER = 'res/chara1_4x.png'
+var key_jump = 32;
 
 /*
  * window.onload
@@ -29,22 +31,71 @@ window.onload = function(){
   // フレームレートの設定
   game.fps = 30;
   
+  // キー(移動以外)の設定
+  game.keybind(key_jump, 'a'); //ジャンプ
+  
+  
   // 画像のロード
   // プログラムで使う画像は全てここで読み込む
-  game.preload([IMG_GROUND]);
+  game.preload([IMG_GROUND, IMG_PLAYER]);
   
   
   game.onload = function() {
     
+    
+    var player = new Sprite(64, 64);
+    player.image = game.assets[IMG_PLAYER];
+    player.x = 100;
+    player.y = 100;
+    game.rootScene.addChild(player);
+    
     // 地面を敷き詰める。
+    var groundList = [];
     for (var x = 0; x < game.width; x += 64) {
       var ground = new Sprite(64, 64);
       ground.image = game.assets[IMG_GROUND];
       ground.x = x;
       ground.y = game.height - 64;
+      
+      // 衝突判定のために配列に保存する。
+      // 衝突させる物体(コイン、アイテムなど)が増えるにつれ重くなるので1枚のデカいテクスチャに変える可能性アリ
+      groundList.push(ground);
+      // 画面に出す。
       game.rootScene.addChild(ground);
     }
-      
+    
+    // 操作キャラのフレーム毎の処理
+    // TODO: spriteクラスを継承してplayerクラスを作り、フィールドを定義したり自作メソッドが呼び出せるようにする。
+    player.addEventListener
+    ("enterframe", function(){
+      this.y += 5;
+     
+     
+      // こじつけくさい(オブジェクト指向っぽくない)からボツ予定
+      var delta = 5;
+      if (game.input.left) {
+        this.x -= delta;
+      }else if (game.input.right) {
+        this.x += delta;
+      }
+     
+     
+      // 壁との衝突判定
+      // 微調整の余地アリ
+      if (this.x < 0) {
+        this.x = 0;
+      }else if(this.x > game.width - this.width) {
+        this.x = game.width - this.width;
+      }
+     
+      // 地面との衝突判定
+      for (var i = 0; i < groundList.length; i++) {
+      var aGround = groundList[i];
+        if (this.intersect(aGround)) {
+          this.y = aGround.y - this.height;
+        }
+      }
+    });
   }
   game.start();
 };
